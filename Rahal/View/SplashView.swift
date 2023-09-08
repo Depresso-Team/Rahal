f02b0d39a45f19cefff22425c2bd52c792f8f2f2
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2023-09-08 22:55:18 +0300

    Implemented MVVM file structure

---
+++ Rahal/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Rahal
+//
+//  Created by Abdelrahman Esmail on 08/09/2023.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
