18c4bd111bd69b2bcf1c4cf720fdebde97a4b5f2
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-09-09 20:12:55 +0300

    Alright now added The home and some models we could use or remove later and home services , login and register services

--- NetworkLayer/Services/LoginServices.swift
+++ NetworkLayer/Services/LoginServices.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+protocol LoginServicable {
+    func login(parms: [String: Any]) async throws -> UserModel
+}
+
+struct LoginServices: LoginServicable, NetworkServices {
+    func login(parms: [String: Any]) async throws -> UserModel {
+        let data = try await request(endPoint: LoginEndPoint.login(parms), imagesData: nil)
+        do {
+            let user = try JSONDecoder().decode(UserModel.self, from: data)
+            return user
+        } catch {
+            throw error
+        }
+    }
+}
--- Rahal/Model/HomeModel/HomeModel.swift
+++ Rahal/Model/HomeModel/HomeModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+// We could use these
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+}
+struct Media {
+    let imageUrl: URL
+    let description: String
+}
+struct Rating {
+    let averageRating: Double
+    let numberOfReviews: Int
+}
+struct OpeningHours {
+    let openTime: String
+    let closeTime: String
+}
+struct ContactInfo {
+    let phoneNumber: String
+    let emailAddress: String
+    let websiteUrl: URL
+}
+struct Availability {
+    let isAvailable: Bool
+    let price: Double
+}
+struct UserFavorites {
+    let isFavorite: Bool
+}
+struct TourCategory {
+    let name: String
+}
+struct LocalizedContent {
+    let languageCode: String
+    let translatedTitle: String
+    let translatedDescription: String
+}
