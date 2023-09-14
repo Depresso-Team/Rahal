647b1c48aa0f6db0be07e98e284f49b279c8d5f6
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2023-09-14 06:57:01 +0300

    Created SearchView TourDetails Under Constraction

--- Rahal/View/SearchView.swift
+++ Rahal/View/SearchView.swift
@@ -9,7 +9,75 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            SearchBar()
+            HStack{
+                Text("Recently Searched").font(.system(size: 18))
+                Spacer()
+                Image(systemName: "trash")
+                    .foregroundColor(.secondary)
+            }
+            .padding(.horizontal,30)
+            .bold()
+            
+            HStack {
+                Text("Giza Pyramids")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Text("Nubia")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Text("Abu Simble Temple")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Spacer()
+                
+            }.padding(.horizontal,30)
+            .padding(.vertical,6)
+            
+            HStack {
+                Text("Nubia")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Text("Giza Pyramids")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Text("Abu Simble Temple")
+                    .font(.footnote)
+                    .padding(10)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .fixedSize()
+                
+                Spacer()
+                
+            }.padding(.horizontal,30)
+            
+            
+            
+            
+            Spacer()
+        }
     }
 }
 
---
+++ Rahal/View/TourDetailsView.swift
@@ -0,0 +1,66 @@
+//
+//  TourDetailsView.swift
+//  Rahal
+//
+//  Created by Abdelrahman Esmail on 14/09/2023.
+//
+
+import SwiftUI
+
+struct TourDetailsView: View {
+    // MARK: - PROPERTIES
+    let rating: Double
+    @State private var currentIndex = 0
+    let tourImages: [String] = ["abuSimbleTemple","sphinx"]
+
+    
+    var body: some View {
+        VStack{
+            
+            TabView {
+                    ForEach(tourImages, id: \.self) { imageName in
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFill()
+                    }
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.45)
+            .tabViewStyle(PageTabViewStyle())
+            
+            
+
+            VStack(alignment: .leading) {
+                Text("Giza Pyramids")
+                    .fontWeight(.heavy)
+                    .font(.system(size: 25))
+                    .padding(.vertical,2)
+                
+                HStack {
+                    ForEach(0..<5) { index in
+                        Image(systemName: index < Int(rating) ? "star.fill" :
+                                (index == Int(rating) && rating.truncatingRemainder(dividingBy: 1) == 0.5 ? "star.lefthalf.fill" : "star"))
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 14))
+                    }.padding(.vertical,2)
+                    
+                    Text("\(rating,specifier: "%.1f")").bold().font(.footnote)
+                    Text("(25000 reviews)")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    
+                    
+                Spacer()
+                }
+            }.padding()
+            
+            
+            Spacer()
+        }
+    }
+}
+
+struct TourDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TourDetailsView(rating: 4.5)
+    }
+}
