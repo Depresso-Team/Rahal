7ce32ee658d38be8a72bd06801438540cdf6bb3c
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-09-15 21:22:24 +0300

    refactored tour guide model and view model and trip details view model , end points are not done

--- Rahal/Model/DetailsModel/TourGuideDetailsModel.swift
+++ Rahal/Model/DetailsModel/TourGuideDetailsModel.swift
@@ -9,7 +9,22 @@ import Foundation
 struct TourGuideDetailsModel: Codable {
     // we will add more depends on the api 
     let id: Int
-    let name: String
-    let bio: String
-    let imageUrl: String 
+    let username: String
+    let personal_photo: String
+    let rate: Double
+    let review: [String]
+    let tour_ids: Int
 }
+
+/*
+ {
+     "id": 9,
+     "username": "ali",
+     "personal_photo": "https://rahal-app-efe3e7eff0b7.herokuapp.com/media/tours/default_photo.jpg",
+     "rate": "0.00",
+     "review": [],
+     "tour_ids": [
+         10
+     ]
+ }
+ */
--- Rahal/ViewModel/DetailsViewModel/TripDetailsViewModel.swift
+++ Rahal/ViewModel/DetailsViewModel/TripDetailsViewModel.swift
@@ -11,19 +11,24 @@ class TripDetailsViewModel {
     static let shared = TripDetailsViewModel()
     private var services: TripDetailsServices!
     
-    var responseHandler: ((_ result: Result<[TripDetailsModel], Error>) -> Void)?
+    @Published var tripDetails: TripDetailsModel? // Change the type to represent a single TripDetailsModel
+    
+    var responseHandler: ((_ result: Result<TripDetailsModel, Error>) -> Void)? // Update the result type
     
     private init() {
         services = TripDetailsServices()
     }
+    
     @MainActor
     func fetchTripDetails(tripID: Int) {
         Task {
             do {
-                let _ = try await services.fetchTripDetails(tripID: tripID)
-//                responseHandler?(true)
+                let tripDetailsData = try await services.fetchTripDetails(tripID: tripID)
+                tripDetails = tripDetailsData // Assign the fetched data to tripDetails
+                responseHandler?(.success(tripDetailsData)) // Use the .success result type
             } catch {
-//                responseHandler?(false)
+                tripDetails = nil // Handle errors by setting tripDetails to nil
+                responseHandler?(.failure(error)) // Use the .failure result type
             }
         }
     }
