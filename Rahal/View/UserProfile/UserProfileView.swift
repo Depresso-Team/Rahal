3121097e9ef201d9acf53f53c4ee4473697d4cec
Abdalazem Salehm <abdalazem.saleh2@icloud.com>
2023-09-16 19:52:53 +0300

    Fix profile view scrollView bug

--- Rahal/View/UserProfile/UserProfileView.swift
+++ Rahal/View/UserProfile/UserProfileView.swift
@@ -30,36 +30,36 @@ struct UserProfileView: View {
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            VStack {
-                Image("profile")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 240)
-                    .ignoresSafeArea()
-                VStack{
-                    // USER PROFILE IMAGE
+            ScrollView {
+                VStack {
+                    Image("profile")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 240)
                     VStack{
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .foregroundColor(Color("CustomDarkGreenColor"))
-                            .frame(width: 100, height: 100)
-                            .clipShape(Circle())
-                        
-                        Text("Abdelrahman Esmail").bold().font(.system(size: 24))
-                            .padding(.top,5)
-                            .padding(.bottom,20)
-                    }
-                    // LIST
-                    VStack(spacing: 8) {
-                        ForEach(profileData) { row in
-                            NavigationLink {
-                                ViewProfileSubView()
-                            } label: {
-                                RowItem(image: row.image, title: row.title)
-                            }
-                            Divider()
+                        // USER PROFILE IMAGE
+                        VStack{
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .foregroundColor(Color("CustomDarkGreenColor"))
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                            
+                            Text("Abdelrahman Esmail").bold().font(.system(size: 24))
+                                .padding(.top,5)
+                                .padding(.bottom,20)
                         }
+                        // LIST
+                        VStack(spacing: 8) {
+                            ForEach(profileData) { row in
+                                NavigationLink {
+                                    ViewProfileSubView()
+                                } label: {
+                                    RowItem(image: row.image, title: row.title)
+                                }
+                                Divider()
+                            }
                             Button {
                                 showLogoutAction = true
                             } label: {
@@ -69,38 +69,40 @@ struct UserProfileView: View {
                                     Text("Logout")
                                 }
                             }
-                        .foregroundColor(.secondary)
-                        .fontWeight(.semibold)
-                        .font(.system(size: 16))
-                        .padding(8)
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .font(.system(size: 16))
+                            .padding(8)
+                        }
+                        .frame(width: UIScreen.main.bounds.width - 80)
+                        .padding(.vertical, 32)
+                        .padding(.horizontal)
+                        .background(.secondary.opacity(0.1))
+                        .cornerRadius(48)
                     }
-                    .frame(width: UIScreen.main.bounds.width - 80)
-                    .padding(.vertical, 32)
-                    .padding(.horizontal)
-                    .background(.secondary.opacity(0.1))
-                    .cornerRadius(48)
+                    .offset(y: -50)
+                    .padding(.bottom, 48)
                 }
-                .offset(y: -50)
-                .padding(.bottom, 48)
+                .actionSheet(isPresented: $showLogoutAction) {
+                    ActionSheet(title: Text("Woops"), message: Text("Are you sure you want logout?"), buttons: [
+                        .destructive(Text("Logout"),
+                                     action: {
+                                         print("Logout")
+                                     }),
+                        .cancel(Text("Cancel"))
+                    ])
+                }
+                .actionSheet(isPresented: $showLogoutAction) {
+                    ActionSheet(title: Text("Woops"), message: Text("Are you sure you want logout?"), buttons: [
+                        .destructive(Text("Logout"),
+                                     action: {
+                                         print("Logout")
+                                     }),
+                        .cancel(Text("Cancel"))
+                    ])
+                }
             }
-            .actionSheet(isPresented: $showLogoutAction) {
-                ActionSheet(title: Text("Woops"), message: Text("Are you sure you want logout?"), buttons: [
-                    .destructive(Text("Logout"),
-                                 action: {
-                        print("Logout")
-                    }),
-                    .cancel(Text("Cancel"))
-                ])
-            }
-            .actionSheet(isPresented: $showLogoutAction) {
-                ActionSheet(title: Text("Woops"), message: Text("Are you sure you want logout?"), buttons: [
-                    .destructive(Text("Logout"),
-                                 action: {
-                        print("Logout")
-                    }),
-                    .cancel(Text("Cancel"))
-                ])
-            }
+            .ignoresSafeArea()
         }
     }
 }
