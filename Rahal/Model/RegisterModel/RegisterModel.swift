a8aa21b65e2da4505571bc8108e762cc9b5b69d1
Abdalazem Salehm <abdalazem.saleh2@icloud.com>
2023-09-17 18:55:11 +0300

    Fic users defults bugs

--- Rahal/Model/RegisterModel/RegisterModel.swift
+++ Rahal/Model/RegisterModel/RegisterModel.swift
@@ -32,36 +32,3 @@ struct RegisterParms {
     let languages: String
     let is_guide: Bool
 }
-
-/*
- case 2002
- {
-     "id": 41,
-     "username": "Abdalazemm",
-     "email": "abdalazem.saleh2002@gmail.com",
-     "phone": 1062690292,
-     "address": "Egypt",
-     "country_code": 1,
-     "photo_url": "https://pbs.twimg.com/profile_images/881713129285185536/wzBq1O-8_400x400.jpg",
-     "languages": "en",
-     "is_guide": false,
-     "session_message": "New user 'Abdalazemm' has been created! Welcome aboard!"
- }
- */
-
-
-/*
- case 406
- {
-     "username": [
-         "This field is required."
-     ],
-     "email": [
-         "user with this email already exists."
-     ],
-     "phone": [
-         "user with this phone already exists."
-     ]
- }
- */
-
